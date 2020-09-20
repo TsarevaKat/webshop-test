7ee9aa7dda6994b76f795e3e6a9cc059fbbb33e6
Царева Екатерина Сергеевна <Tsareva_ES@cd.local>
2020-09-20 15:21:55 +0300

    chore: config webpack

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,19 +1,9 @@
 const {merge} = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
-const WebpackFtpUpload = require('webpack-ftp-upload-plugin');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
-  plugins: [
-  //   new WebpackFtpUpload({
-  //     host: '89.108.85.65',
-  //     port: '21',
-  //     username: 'katet771',
-  //     password: 'pFvU80eRZl',
-  //     local: baseWebpackConfig.externals.paths.dist,
-  //     path: '/www/tsarevakat.ru/politbook',
-  // })
-  ]
+  plugins: []
 });
 
 module.exports = new Promise((resolve, reject) => {
